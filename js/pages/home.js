fb940082718cdfde4552293dcad4110d4c2e8f95
RokasAbra <rokasabramavicius@gmail.com>
2022-04-12 14:01:30 +0300

    Git conflicts abd layout

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,3 +1,48 @@
 // component imports
 
 // components execution
+
+/* header: start */
+/* header: end */
+
+/* hero: start */
+/* hero: end */
+
+/* premium features: start */
+/* premium features: end */
+
+/* comunication app: start */
+/* comunication app: end */
+
+/* device manage: start */
+/* device manage: end */
+
+/* how app works: start */
+/* how app works: end */
+
+/* interface slide: start */
+/* interface slide: end */
+
+/* testimonials: start */
+/* testimonials: end */
+
+/* pricing: start */
+/* pricing: end */
+
+/* FAQ: start */
+/* FAQ: end */
+
+/* team: start */
+/* team: end */
+
+/* app store: start */
+/* app store: end */
+
+/* subscribe: start */
+/* subscribe: end */
+
+/* contacts: start */
+/* contacts: end */
+
+/* footer: start */
+/* footer: end */

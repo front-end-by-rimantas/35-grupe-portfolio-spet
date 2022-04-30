b7ddeed2497a27a38faf4baa1c706723e26ecb05
SergejsRom <sergejsromanovskis@gmail.com>
2022-04-30 22:24:10 +0300

    responsive

--- js/components/news.js
+++ js/components/news.js
@@ -4,25 +4,28 @@ function renderNews(selector, data) {
 
     for (const item of data) {
         HTML += `
-    <div class="col-12 col-sm-6 col-md-4>
-    <div>
-              <a href="#"><img src="${item.scr}" alt="${item.alt}" /></a>
-        </div>
+        <div class="col-sm-12 col-md-6 ml-0 col-lg-4">
         <div>
-              <ul>
-                <li>
-                  <a href="#">${item.admin} </a>
-                </li>
-                <li>${item.day}</li>
-              </ul>
-              <h3>
-                <a href="#"> ${item.header} </a>
-              </h3>
+          <div>
+            <a href="#"><img src="${item.scr}" alt="${item.alt}" /></a>
+          </div>
+          <div>
+            <ul>
+              <li>
+                <a href="#">${item.admin} </a>
+              </li>
+              <li>${item.day}</li>
+            </ul>
+            <h3>
+              <a href="#"> ${item.header} </a>
+            </h3>
             <a href="#">${item.more}</a>
-            </div>
-    </div>
+          </div>
+        </div>
+      </div>
           `;
     }
+        
 
     DOM.innerHTML = HTML;
 
